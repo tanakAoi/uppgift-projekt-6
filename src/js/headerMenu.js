0094062571b596f761564bfbda3e579ce4f3ea66
Aoi Tanaka <aoi.tanaka@studerande.karlskogafhs.se>
2023-04-19 15:27:10 +0200

    Add hover effect to sections

--- src/js/headerMenu.js
+++ src/js/headerMenu.js
@@ -2,14 +2,14 @@ const siteMenu = () => {
 
     // Get elements from DOM
     const menuButton = document.querySelector('.menu-button')
-    const headerMenuList = document.querySelector('.header-menu__list')
+    // const headerMenuList = document.querySelector('.header-menu__list')
     const siteMenu = document.querySelector('.site-menu')
 
     // Toggle header menu
-    const toggleHeaderMenu = (event) => {
-        siteMenu.classList.toggle('site-menu--visible')
+    const toggleHeaderMenu = () => {
         menuButton.classList.toggle('menu-button--close')
-        headerMenuList.classList.toggle('header-menu__list--invisible')
+        // headerMenuList.classList.toggle('header-menu__list--invisible')
+        siteMenu.classList.toggle('site-menu--visible')
     }
     
     // Create Events
